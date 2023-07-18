0c28802d9389eeaba645550323e5cc042b1b514a
santiago <santiagomahecha192@gmail.com>
2023-07-18 11:44:03 -0500

    clase 03

---
+++ src/main/java/Validaciones/UsuarioValidacion.java
@@ -0,0 +1,17 @@
+package Validaciones;
+
+public class UsuarioValidacion {
+
+    // Metodos Ordinarios
+    public Boolean validarNombre(String nombre){
+
+        return true;
+    }
+    public Boolean validarUbicacion(Integer ubicacion){
+        return true;
+    }
+    public Boolean validarCorreo(String correo){
+        return true;
+    }
+
+}
